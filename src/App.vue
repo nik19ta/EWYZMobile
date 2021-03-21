28aa5380bf80233d00062b17407d737124978fa0
nik19ta <nikhvatov19@gmail.com>
2021-03-21 12:14:08 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <FirsScreen msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FirsScreen from './components/FirsScreen.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FirsScreen
   }
 }
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;600;700&display=swap');
+
+body {
+  margin: 0;
+  padding: 0;
+
+  min-height: 100vh;
+  width: 100vw;
+}
+
+*, *::after, *::before {
+  font-family: 'Montserrat', sans-serif;
+  box-sizing: border-box;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  min-height: 100vh;
+  height: 100%;
+  width: 100%;
+  background: #212121;
 }
 </style>
---
+++ src/components/FirsScreen.vue
@@ -0,0 +1,193 @@
+<template>
+  <div class="FirsScreen">
+    <div class="container_logo">
+      <img class="logo" src="../assets/logo.svg" alt="">
+    </div>
+    <div class="container_mobile_photo">
+      <img v-show="step == 1" class="mobile_photo" src="../assets/screen_1.png" alt="">
+      <img v-show="step == 2" class="mobile_photo" src="../assets/screen_2.png" alt="">
+      <img v-show="step == 3" class="mobile_photo" src="../assets/screen_3.png" alt="">
+    </div>
+    <div class="container_title">
+      <h1>
+        {{step == 1 ? "Сканируй QR-Code" : ""}}
+        {{step == 2 ? "Просматривай меню" : ""}}
+        {{step == 3  ? "Смотри в 3D" : ""}}
+      </h1>
+
+      <div class="steps">
+        <span :style="step > 0 ? 'opacity: 1' : 'opacity: 0.5' " class="step steps1"></span>
+        <span :style="step > 1 ? 'opacity: 1' : 'opacity: 0.5' " class="step steps2"></span>
+        <span :style="step > 2 ? 'opacity: 1' : 'opacity: 0.5' " class="step steps3"></span>
+      </div>
+
+      <div class="btns">
+        <div class="btns_group">
+          <button @click="backStap" class="btn btn_cancel">
+            <img class="img_in_btn" :src="step > 1 ? require('../assets/back.png') : require('../assets/cancel.png')" alt="">
+            </button>
+          <button @click="nextStap" class="btn btn_to">
+            <img class="img_in_btn" src="../assets/to.png" alt="">
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'FirsScreen',
+    data() {
+      return {
+        step: 1,
+      }
+    },
+    methods: {
+      nextStap() {
+        if (this.step == 1) this.step++
+        else if (this.step == 2) this.step++
+      },
+      backStap() {
+        if (this.step == 1) return null
+        else if (this.step == 2) this.step = 1
+        else if (this.step == 3) this.step--
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  .FirsScreen {
+    width: 100vw;
+    height: 100vh;
+
+    display: flex;
+    flex-wrap: wrap;
+
+    .container_logo {
+      width: 100%;
+      margin-top: 10px;
+
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      .logo {
+        width: 108px;
+      }
+    }
+
+    .container_mobile_photo {
+      width: 100%;
+
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      .mobile_photo {
+        max-height: calc(100vh - 300px);
+        max-width: 90vw;
+      }
+    }
+
+    .container_title {
+      width: 100%;
+      // height: 100px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-wrap: wrap;
+
+      h1 {
+        font-family: Montserrat;
+        font-style: normal;
+        font-weight: 600;
+        font-size: 22px;
+        line-height: 88px;
+        color: #FFFFFF;
+        text-align: center;
+
+        margin-top: -20px;
+      }
+
+      .steps{
+        width: 100%;
+        height: 20px;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        margin-top: -60px;
+        z-index: 2;
+
+        .step{
+          width: 8px;
+          height: 8px;
+
+          display: block;
+          border-radius: 10px;
+          background: #fff;
+          opacity: 0.5;
+          margin-left: 9px;
+          margin-right: 9px;
+        }
+      }
+
+      .btns {
+        width: 100%;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        margin-top: -20px;
+        margin-bottom: 20px;
+
+        .btn_cancel {
+          background: #000000;
+
+          width: 40px;
+          height: 40px;
+
+          border-radius: 100%;
+          padding: 10px;
+          margin-right: 5px;
+        }
+
+        .btn_to {
+          background: #fff;
+
+          width: 60px;
+          height: 60px;
+
+          border-radius: 100%;
+          padding: 10px;
+          margin-left: 5px;
+        }
+
+        .btns_group {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+
+          margin-left: -50px;
+        }
+
+        .btn {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+
+          border: none;
+          cursor: pointer;
+        }
+
+        .img_in_btn {
+          width: 100%;
+        }
+      }
+    }
+  }
+</style>
