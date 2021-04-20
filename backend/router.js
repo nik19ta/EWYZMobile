a0049d4452e1c32a4aeb043f2b31313583058e76
nik19ta <nikhvatov19@gmail.com>
2021-04-20 18:32:11 +0300

    add backend

--- backend/router.js
+++ backend/router.js
@@ -8,15 +8,31 @@ router.use(function timeLog(req, res, next) {
   next();
 });
 // define the home page route
-router.get(`/get_menu`, async function(req, res) {
+router.get(`/get_menu`, async function (req, res) {
+  let data = JSON.parse(fs.readFileSync('./db.json'))
 
-    let data = JSON.parse(fs.readFileSync('./db.json'))
-
-    res.send(data.menu[req._parsedUrl.query]);
+  res.send(data.menu[req._parsedUrl.query.split('id=')[1]]);
 });
+
+
+router.get('/search', function (req, res) {
+  let data = JSON.parse(fs.readFileSync('./db.json'))['menu'][req._parsedUrl.query.split("id=")[1]]
+  
+  let products = []
+  for (let item in data) {
+    for (let i = 0; i < data[item]['items'].length; i++) {
+      if (data[item]['items'][i]['name'] == decodeURI(req._parsedUrl.query.split('name=')[1].split('&')[0])) {
+        products.push(data[item]['items'][i]);
+      }
+    }
+  }
+  res.send({data: products});
+});
+
 // define the about route
-router.get('/about', function(req, res) {
+router.get('/about', function (req, res) {
   res.send('About');
 });
+
 
 module.exports = router;
